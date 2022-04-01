fee8a931f02ea6a7fefcef81fcc6a4c4ace7a1e3
Christian314 <charlygordon14@gmail.com>
2022-04-01 12:31:35 +0200

    added slider input for grid size

--- sketchboard.js
+++ sketchboard.js
@@ -1,6 +1,18 @@
 const sketchArea = document.getElementById("sketchArea")
 const reset = document.getElementById("reset")
 
+//changing the grid size by using slider & display current size
+const sizeValue = document.getElementById("sizeValue")
+const gridSlider = document.getElementById("gridslider")
+
+gridSlider.onmousemove = (e) => updateSizeValue(e.target.value)
+gridSlider.onchange = (e) => createSketchArea(e.target.value)
+
+function updateSizeValue(value) {
+    sizeValue.innerHTML = `${value} x ${value}`
+}
+
+//create the Area
 function createSketchArea(width) {
     sketchArea.innerHTML = "";
     for (let i = 1; i <= width**2; i++) {
@@ -14,8 +26,9 @@ function createSketchArea(width) {
         sketchArea.appendChild(divs);
     }
 }
-
+//reset the Area
 reset.addEventListener("click", function(){
-    let numberOfSquares = prompt("Enter a number between 1 and 64");
-    createSketchArea(numberOfSquares);
+    createSketchArea(gridSlider.value)
 });
+
+createSketchArea(8);
